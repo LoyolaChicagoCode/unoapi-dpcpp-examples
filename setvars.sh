83d0fd24def3cafef5632e4dc020dad355edfe2d
Konstantin Läufer <laufer@cs.luc.edu>
2022-06-07 11:30:36 -0700

    updated build configuration to support GoogleTest
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- setvars.sh
+++ setvars.sh
@@ -2,4 +2,9 @@ source /opt/intel/oneapi/setvars.sh
 
 export CC=icx
 export CXX=dpcpp
+
+# required for compiling GoogleTest
+export CFLAGS="-fp-model=precise"
+export CXXFLAGS="-fp-model=precise"
+
 PATH=/opt/intel/oneapi/compiler/latest/linux/bin:$PATH
