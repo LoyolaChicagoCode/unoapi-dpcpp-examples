b0a00526e394625a3b88e9de2581cd1e4c3a9630
Konstantin Läufer <laufer@cs.luc.edu>
2022-07-13 11:40:35 -0500

    minor reorganization of integration code for clarity
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- integration/main.cpp
+++ integration/main.cpp
@@ -14,7 +14,7 @@
 
 double f(double x);
 
-int main(const int argc, const char *const argv[]) {
+int main(const int argc, const char * const argv[]) {
     constexpr size_t DEFAULT_NUMBER_OF_TRAPEZOIDS{10};
     size_t number_of_trapezoids{DEFAULT_NUMBER_OF_TRAPEZOIDS};
     double x_min{0.0};
@@ -42,62 +42,75 @@ int main(const int argc, const char *const argv[]) {
 
     spdlog::info("integrating function from {} to {} using {} trapezoid(s), dx = {}", x_min, x_max, number_of_trapezoids, dx);
 
-    std::vector values(size, 0.0);
-    double result{0};
+    double result{0.0};
 
     if (run_sequentially) {
+        std::vector values(size, 0.0);
+
         spdlog::info("starting sequential integration");
 
+        // populate vector with function values and add trapezoid area to result
         values[0] = f(x_min);
-        for (auto index = 1L; index <= number_of_trapezoids; index++) {
-            // x = x_min + index * dx
-            //   = x_min + index * (x_max - x_min) / number_of_trapezoids
-            //   = (x_min * number_of_trapezoids + index * (x_max - x_min)) / number_of_trapezoids
-            const auto x = (x_min * (number_of_trapezoids - index) + x_max * index) / number_of_trapezoids;
-            values[index] = f(x);
-            result += trapezoid(values[index - 1], values[index], half_dx);
+        for (auto i{1UL}; i <= number_of_trapezoids; i++) {
+            // x = x_min + i * dx
+            //   = x_min + i * (x_max - x_min) / number_of_trapezoids
+            //   = (x_min * number_of_trapezoids + i * (x_max - x_min)) / number_of_trapezoids
+            const auto x = (x_min * (number_of_trapezoids - i) + x_max * i) / number_of_trapezoids;
+            values[i] = f(x);
+            result += trapezoid(values[i - 1], values[i], half_dx);
+        }
+
+        if (show_function_values) {
+            for (auto i{0UL}; i < size; i++) {
+                fmt::print("{}: f({}) = {}\n", i, x_min + i * dx, values[i]);
+            }
+            fmt::print("\n");
         }
     } else {
+        // important: buffer NOT backed by host-allocated vector
+        // this allows the data to live on the device until accessed on the host (if desired)
+        sycl::buffer<double> v_buf{sycl::range<1>{size}};
+        sycl::buffer<double> r_buf{&result, 1};
+
         spdlog::info("preparing for vectorized integration");
 
         // we use an in-order queue for this simple, sequential computation
         sycl::queue q{sycl::default_selector{}, dpc_common::exception_handler, sycl::property::queue::in_order()};
         spdlog::info("Device: {}", q.get_device().get_info<sycl::info::device::name>());
 
-        // important: buffer NOT backed by host-allocated vector
-        // this allows the data to live on the device until accessed on the host (if desired)
-        sycl::buffer<double> v_buf{sycl::range<1>{size}};
-        sycl::buffer<double> r_buf{&result, 1};
-
-        q.submit([&](auto &h) {
-            const auto v{v_buf.get_access<sycl::access_mode::write>(h)};
-            h.parallel_for(size, [=](const auto index) {
+        // populate buffer with function values
+        q.submit([&](auto & h) {
+            sycl::accessor v{v_buf, h};
+            h.parallel_for(size, [=](const auto & index) {
                 const double x = (x_min * (number_of_trapezoids - index) + x_max * index) / number_of_trapezoids;
                 v[index] = f(x);
             });
         });
 
-        q.submit([&](auto &h) {
-            const auto v{v_buf.get_access<sycl::access_mode::read>(h)};
+        // perform reduction into result
+        q.submit([&](auto & h) {
+            sycl::accessor v{v_buf, h};
             const auto sum_reduction{sycl::reduction(r_buf, h, sycl::plus<>())};
-            h.parallel_for(sycl::range<1>{number_of_trapezoids}, sum_reduction, [=](const auto index, auto &sum) {
+            h.parallel_for(sycl::range<1>{number_of_trapezoids}, sum_reduction, [=](const auto & index, auto & sum) {
                 sum.combine(trapezoid(v[index], v[index + 1], half_dx));
             });
         });
 
         spdlog::info("done submitting to queue...waiting for results");
+
+        if (show_function_values) {
+            sycl::host_accessor values{v_buf};
+
+            spdlog::info("showing function values");
+
+            for (auto i{0UL}; i < size; i++) {
+                fmt::print("{}: f({}) = {}\n", i, x_min + i * dx, values[i]);
+            }
+            fmt::print("\n");
+        }
     }
     spdlog::info("results should be available now");
 
-    if (show_function_values) {
-
-        // TODO explicitly copy from device (using host_accessor?)
-
-        for (auto i{0UL}; i < size; i++) {
-            fmt::print("{}: f({}) = {}\n", i, x_min + i * dx, values[i]);
-        }
-        fmt::print("\n");
-    }
     fmt::print("result = {}\n", result);
 
     return 0;
