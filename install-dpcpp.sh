b31103d30f2fbc1eee432de82a3bce05b04d3ca3
Konstantin Läufer <laufer@cs.luc.edu>
2022-08-16 21:58:53 +0000

    install-dpcpp.sh
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

---
+++ install-dpcpp.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Prerequisite: recent Ubuntu environment
+
+echo Add Intel Apt repository
+
+wget -qO- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | sudo tee /etc/apt/trusted.gpg.d/GPG-PUB-KEY-INTEL-SW-PRODUCTS.asc
+echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+sudo apt update
+        
+echo Install Intel oneAPI
+
+sudo apt install intel-oneapi-compiler-dpcpp-cpp
+
+echo Done
