905b0d2b8d694d3041c8db7a482d743557dd73d4
Konstantin Läufer <laufer@cs.luc.edu>
2022-08-16 18:45:16 -0500

    simplified implementation of --cpu-only buffer option, fixes #6
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- integration/main.cpp
+++ integration/main.cpp
@@ -24,7 +24,7 @@ int main(const int argc, const char * const argv[]) {
     double x_max{1.0};
     bool show_function_values{false};
     bool run_sequentially{false};
-    bool run_hostonly{false};
+    bool run_cpuonly{false};
     uint x_precision{1};
     uint y_precision{1};
 
@@ -35,7 +35,7 @@ int main(const int argc, const char * const argv[]) {
     app.add_option("-u,--upper,--xmax", x_max, "x max value");
     app.add_flag("-v,--show-function-values", show_function_values);
     app.add_flag("-s,--sequential", run_sequentially);
-    app.add_flag("-o,--host-only", run_hostonly);
+    app.add_flag("-c,--cpu-only", run_cpuonly);
     app.add_option("-x,--x-format-precision", x_precision, "decimal precision for x values")->check(CLI::PositiveNumber.description(" >= 1"));
     app.add_option("-y,--y-format-precision", y_precision, "decimal precision for y (function) values")->check(CLI::PositiveNumber.description(" >= 1"));
     CLI11_PARSE(app, argc, argv);
@@ -71,32 +71,6 @@ int main(const int argc, const char * const argv[]) {
             spdlog::info("showing function values");
             print_function_values(values, x_min, dx, x_precision, y_precision);
         }
-    } else if (run_hostonly) {
-        // TODO discuss whether this is valuable because we are losing SYCL's reduction semantics
-        sycl::buffer<double> v_buf{sycl::range<1>{size}};
-        sycl::buffer<double> r_buf{sycl::range<1>{1}};
-
-        spdlog::info("starting host-only integration");
-
-        // populate buffer with function values
-        const sycl::host_accessor v{v_buf};
-        for (auto index{0UL}; index < size; index++) {
-            v[index] = f(x_min + index * dx);
-        }
-
-        // perform reduction into result
-        const sycl::host_accessor result{r_buf};
-        for (auto index{0UL}; index < number_of_trapezoids; index++) {
-            result[0] += trapezoid(v[index], v[index + 1], half_dx);
-        }
-
-        spdlog::info("result should be available now");
-        fmt::print("result = {}\n", result[0]);
-
-        if (show_function_values) {
-            spdlog::info("showing function values");
-            print_function_values(v, x_min, dx, x_precision, y_precision);
-        }
     } else {
         // important: buffer NOT explicitly backed by host-allocated vector
         // this allows the data to live on the device until accessed on the host (if desired)
@@ -105,8 +79,16 @@ int main(const int argc, const char * const argv[]) {
 
         spdlog::info("preparing for vectorized integration");
 
+        sycl::device_selector * device_selector = nullptr;
+
+        if (run_cpuonly) {
+            device_selector = new sycl::cpu_selector{};
+        } else {
+            device_selector = new sycl::default_selector{};
+        }
+
         // we use an in-order queue for this simple, sequential computation
-        sycl::queue q{sycl::default_selector{}, dpc_common::exception_handler, sycl::property::queue::in_order()};
+        sycl::queue q{*device_selector, dpc_common::exception_handler, sycl::property::queue::in_order()};
         spdlog::info("Device: {}", q.get_device().get_info<sycl::info::device::name>());
 
         // populate buffer with function values
@@ -138,6 +120,8 @@ int main(const int argc, const char * const argv[]) {
             spdlog::info("showing function values");
             print_function_values(values, x_min, dx, x_precision, y_precision);
         }
+
+        delete device_selector;
     } // end of scope waits for the queued work to complete
 
     spdlog::info("all done for now");
