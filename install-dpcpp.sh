bbf0ed079be25a594a9730bb51d1e358750d4e4d
Konstantin Läufer <laufer@cs.luc.edu>
2022-08-31 02:55:00 +0000

    removed prompting from auto-config and install scripts
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- install-dpcpp.sh
+++ install-dpcpp.sh
@@ -6,10 +6,10 @@ echo Add Intel Apt repository
 
 wget -qO- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | sudo tee /etc/apt/trusted.gpg.d/GPG-PUB-KEY-INTEL-SW-PRODUCTS.asc
 echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
-sudo apt update
+sudo apt update -q
         
 echo Install Intel oneAPI
 
-sudo apt install intel-oneapi-compiler-dpcpp-cpp
+sudo apt install -q -y intel-oneapi-compiler-dpcpp-cpp
 
 echo Done
