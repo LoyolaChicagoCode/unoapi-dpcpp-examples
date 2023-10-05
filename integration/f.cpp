cacac6f16b185a078ab65825e6c7908e5eb0b7f7
Konstantin Läufer <laufer@cs.luc.edu>
2023-10-05 13:29:22 -0700

    made integrand function more inefficient by a factor of 1000
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- integration/f.cpp
+++ integration/f.cpp
@@ -2,6 +2,10 @@
 
 // {{UnoAPI:f-implementation:begin}}
 double f(const double x) {
-    return cos(x) * cos(x) + sin(x) * sin(x);
+    auto result = 1.0;
+    for (auto n = 0; n < 1000; n++) {
+        result *= cos(x) * cos(x) + sin(x) * sin(x);
+    }
+    return result;
 }
 // {{UnoAPI:f-implementation:end}}
