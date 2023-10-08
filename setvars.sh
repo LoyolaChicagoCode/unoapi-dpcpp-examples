ef5a217b3e54233f0a81b9c44942f436d24e90b8
Konstantin Läufer <laufer@cs.luc.edu>
2023-10-08 01:34:08 -0500

    updated setvars.sh to LLVM toolchain
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- setvars.sh
+++ setvars.sh
@@ -1,12 +1,12 @@
 source /opt/intel/oneapi/setvars.sh --force --include-intel-llvm
 
-export CC=icx
-export CXX=icpx
+export CC=clang
+export CXX=clang++
 
 # required for compiling GoogleTest
 export CFLAGS="-fp-model=precise"
 export CXXFLAGS="-fsycl -fp-model=precise"
 
-PATH=/opt/intel/oneapi/compiler/latest/linux/bin:$PATH
+PATH=/opt/intel/oneapi/compiler/latest/linux/bin-llvm:$PATH
 
 echo ":: set project-specific environment variables"
