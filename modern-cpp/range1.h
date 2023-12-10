5122667c1aa43d9577dacc02311262427f11d198
Konstantin Läufer <laufer@cs.luc.edu>
2023-12-10 02:21:53 +0000

    removed modern-cpp subdirectory (now separate repo), resolved #20
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- modern-cpp/range1.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by George K. Thiruvathukal on 3/19/23.
-//
-
-#ifndef UNOAPI_DPCPP_EXAMPLES_RANGE1_H
-#define UNOAPI_DPCPP_EXAMPLES_RANGE1_H
-
-#endif //UNOAPI_DPCPP_EXAMPLES_RANGE1_H
