a39fc24fd34f7fdab2c1f7827e5dbd000ee2c061
rhasler1 <rhasler@luc.edu>
2024-02-25 14:47:42 -0600

    updates\n\n1. adding encode/decode options\n2. adding file input/output options\n3. integrating read/write functionality\n4. integrating encode/decode functionality

--- cipher/main.cpp
+++ cipher/main.cpp
@@ -11,79 +11,95 @@
 
 #include "scramble.h"
 #include "timestamps.h"
-#include "readfile.h"
-
+#include "file_ops.h"
 
 int main(const int argc, const char *const argv[])
 {
-    // {{UnoAPI:main-declarations:begin}}
-    std::string msg{"abcdefg"};
-    std::string key{"dogs"};
+    // main declarations begin
+    std::string msg{"abcdefghijklmnopqrstuvwxyz"};
+    std::string key{"Cyndy"};
+    std::vector<char> substitution_alphabet;
     std::string perf_output;
     bool run_sequentially{false};
-    std::string text_file_path;
+    bool encode{false};
+    bool decode{false};
+    std::string input_file_path;
+    std::string output_file_path;
     ts_vector timestamps;
     std::string device_name;
-    // {{UnoAPI:main-declarations:end}}
+    // main declarations end
 
-    // {{UnoAPI:main-cli-setup-and-parse:begin}}
+    // cli setup and parse begin
     CLI::App app{"Substitution cipher"};
     app.option_defaults()->always_capture_default(true);
     app.add_option("-k, --key", key, "key value");
     app.add_option("-m, --msg", msg, "message");
-    app.add_option("-f, --file", text_file_path)
+    app.add_option("-i, --inputfile", input_file_path)
+        ->check(CLI::ExistingFile);
+    app.add_option("-o, --outputfile", output_file_path)
         ->check(CLI::ExistingFile);
     app.add_flag("-s, --sequential", run_sequentially);
+    app.add_flag("-e, --encode", encode);
+    app.add_flag("-d, --decode", decode);
     CLI11_PARSE(app, argc, argv);
-    //{{UnoAPI:main-cli-setup-and-parse:end}}
+    // cli setup and parse end
 
-    // if file option, then load contents of file to string::msg
-    if (text_file_path.size() > 0) {
-        msg = file_to_string(text_file_path);
+    // must encode or decode
+    if (encode == decode) {
+        spdlog::error("must specify -e --encode || -d --decode");
+        return 1;
     }
-    // end reading file to msg
+    
+    // file takes precendence over inline
+    if (input_file_path.size() > 0) {
+        msg = read_from_binary(input_file_path);
+    }
 
-    // {{UnoAPI:main-domain-setup:begin}}
-    const auto key_size = key.size();
+    // main domain setup begin
     const auto msg_size = msg.size();
-    const auto ascii_a{97};
-    const auto alphabet_range{26};
-    // {{UnoAPI:main-domain-setup:end}}
-
-    mark_time(timestamps, "Start");
-
-    // {{UnoAPI:main-sequential-option:begin}}
+    const auto character_base{0};
+    const auto character_range{128};
+    // main domain setup end
+    
+    
+    mark_time(timestamps, "start");
+    // initializing alphabet begin
+    spdlog::info("preparing alphabet vector");
+    if (encode) {
+        substitution_alphabet = scramble(key, character_base, character_range);
+    }
+    else {
+        substitution_alphabet = unscramble(key, character_base, character_range);
+    }
+    mark_time(timestamps, "initializing alphabet vector");
+    // initializing alphabet end
+    
+    // run sequential begin
     if (run_sequentially) {
         device_name = "sequential";
-	std::vector<char> cipher_alphabet = scramble(key, key_size, ascii_a, alphabet_range);
-	std::vector<char> result(msg_size);
-	mark_time(timestamps,"Scramble alphabet");
+	    std::vector<char> result(msg_size);
+        mark_time(timestamps, "memmory allocation");
 
-
-	spdlog::info("starting sequential encoding");
-	for (int i = 0; i < msg_size; i++) {
-	    result[i] = cipher_alphabet[msg[i] - ascii_a];
-	}
-	mark_time(timestamps,"encoding plaintext");
-
-
-	spdlog::info("results should be available now");
-	//fmt::print("ciphertext = {}\n", result);
+	    spdlog::info("starting sequential encoding");
+	    for (int i = 0; i < msg_size; i++) {
+	        result[i] = substitution_alphabet[msg[i] - character_base];
+	    }
+	    mark_time(timestamps,"encoding plaintext");
+        if (output_file_path.size() > 0) {
+            spdlog::info("starting to write result to output file");
+            write_to_binary(result, output_file_path);
+        }
+        mark_time(timestamps,"writing to file");
+	    spdlog::info("results should be available now");
     }
-    // {{UnoAPI:main-sequential-option:end}}
+    // run sequential end
 
     else {
-        std::vector<char> cipher_alphabet = scramble(key, key_size, ascii_a, alphabet_range);
-        mark_time(timestamps, "Scramble alphabet");
-        spdlog::info("prepared ciphertext alphabet");
-
-
         sycl::buffer<char> msg_buf{msg.data(), sycl::range<1>{msg_size}};
-        sycl::buffer<char> cipher_alphabet_buf{cipher_alphabet.data(), sycl::range<1>{alphabet_range}};
+        sycl::buffer<char> substitution_alphabet_buf{substitution_alphabet.data(), sycl::range<1>{character_range}};
         sycl::buffer<char> result_buf{sycl::range<1>{msg_size}};
-        mark_time(timestamps, "Memmory allocation");
+        mark_time(timestamps, "memmory allocation");
         spdlog::info("starting parallel encoding");
-
 
         sycl::device device {sycl::default_selector_v};
         sycl::queue Q{device};
@@ -91,41 +107,29 @@ int main(const int argc, const char *const argv[])
         device_name = Q.get_device().get_info<sycl::info::device::name>();
         spdlog::info("Device: {}", device_name);
 
-
-        /* creating ciphertext */
+        // performing substitution
         Q.submit([&](auto & h) {
-            const sycl::accessor cipher_alphabet{cipher_alphabet_buf, h};
+            const sycl::accessor substitution_alphabet{substitution_alphabet_buf, h};
             const sycl::accessor msg{msg_buf, h};
             const sycl::accessor result{result_buf, h};
             h.parallel_for(msg_size, [=](const auto & i) {
-                result[i] = cipher_alphabet[msg[i] - ascii_a];
+                result[i] = substitution_alphabet[msg[i] - character_base];
             });
         });
         spdlog::info("done submitting to queue...waiting for results");
         mark_time(timestamps,"encoding plaintext");
     
-
+        // host accessor to synchronize memory
         const sycl::host_accessor result{result_buf};
-        const sycl::host_accessor cipher_alphabet_acc{cipher_alphabet_buf};
         mark_time(timestamps,"Host data access");
-
-
+        
         spdlog::info("printing plaintext, key, ciphertext");
-        /* printing plaintext */
-        //std::cout << "plaintext = " << msg;
-        //std::cout << std::endl;
-        /* printing keys */
-        //std::cout << "key = " << key;
-        //std::cout << std::endl;
-        /* printing ciphertext */
-        //std::cout << "ciphertext = ";
-        //for (size_t i = 0; i < result.size(); i++) {
-        //    std::cout << result[i];
-        //}
-        //std::cout << std::endl;
         mark_time(timestamps,"Output");
+        if (output_file_path.size() > 0) {
+            write_to_binary(result, output_file_path);
+        }
     }
-    mark_time(timestamps, "DONE");
+    mark_time(timestamps,"DONE");
 
     spdlog::info("all done for now");
     print_timestamps(timestamps, perf_output, device_name);
