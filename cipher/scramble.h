b36585f767bd465b7c06b8969cd09c787c29968d
rhasler1 <rhasler@luc.edu>
2024-04-01 15:01:01 -0500

    add documentation

--- cipher/scramble.h
+++ cipher/scramble.h
@@ -1,18 +1,28 @@
 #ifndef SCRAMBLE_H
 #define SCRAMBLE_H
 
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <set>
+
+// note: Argument key is passed by value this way the key object in main is unaffected
+// by function calls to remove_duplicates from within scramble and unscramble.
 std::vector<std::byte> scramble(
-    std::string keyword,
-    const int character_base,
-    const int character_range
+    std::vector<std::byte> key,
+    const int decimal_begin,
+    const int decimal_end
 );
 
 std::vector<std::byte> unscramble(
-    std::string keyword,
-    const int character_base,
-    const int character_range
+    std::vector<std::byte> key,
+    const int decimal_begin,
+    const int decimal_end
 );
 
-void remove_duplicates(std::string& this_keyword);
+// note: Argument key is passed by reference here.
+void remove_duplicates(
+    std::vector<std::byte> & key
+);
 
 #endif
