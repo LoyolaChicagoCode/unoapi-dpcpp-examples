11e3a8938aa569b7634629025cac75a1abc196bf
rhasler1 <rhasler@luc.edu>
2024-04-01 14:53:41 -0500

    fix return type
    - update return type to int

--- cipher/file_ops.cpp
+++ cipher/file_ops.cpp
@@ -1,49 +1,18 @@
-#include <sycl/sycl.hpp>
-#include <dpc_common.hpp>
-
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-
-#include <fstream>
-#include <iterator>
-#include <algorithm>
-#include <iostream>
-#include <filesystem>
-#include <cstdint>
-
 #include "file_ops.h"
 
-void read_from_binary(std::vector<std::byte> & buffer, std::string path_to_file)
+// If the file cannot be opened, then the function will notify the caller
+// by returning 1.
+int read_from_binary(std::vector<std::byte> & buffer, std::string path_to_file)
 {
     std::ifstream input(path_to_file, std::ios::binary);
     if (!input.is_open()) {
-        spdlog::info("Error opening file to read from: {}", path_to_file);
-        exit(1);
+        spdlog::error("Error opening file to read from: {}", path_to_file);
+        return 1;
     }
     std::uintmax_t size = std::filesystem::file_size(path_to_file);
     buffer.resize(size);
+    // TODO: See if it is possible to read binary data from file without casting.
     input.read(reinterpret_cast<char*>(buffer.data()), size);
     input.close();
-}
-
-void write_to_binary(const sycl::host_accessor<std::byte, 1> & buffer, std::string path_to_file)
-{
-    std::ofstream output(path_to_file, std::ios::binary);
-    if (!output.is_open()) {
-        spdlog::info("Error opening file to read from: {}", path_to_file);
-        exit(1);
-    }
-    output.write(reinterpret_cast<char*>(&buffer[0]), buffer.size());
-    output.close();
-}
-
-void write_to_binary(std::vector<std::byte> & buffer, std::string path_to_file)
-{
-    std::ofstream output(path_to_file, std::ios::binary);
-    if (!output.is_open()) {
-        spdlog::info("Error opening file to read from: {}", path_to_file);
-        exit(1);
-    }
-    output.write(reinterpret_cast<char*>(&buffer[0]), buffer.size());
-    output.close();
+    return 0;
 }
