f229c3e533d9fcaf2953fefcda719260695798c9
rhasler1 <rhasler@luc.edu>
2024-04-30 14:37:43 -0500

    adding nanotimer support

--- cipher/timestamps.h
+++ cipher/timestamps.h
@@ -1,14 +1,14 @@
-#ifndef INTEGRATION_TIMESTAMPS_H
-#define INTEGRATION_TIMESTAMPS_H
+#ifndef CIPHER_TIMESTAMPS_H
+#define CIPHER_TIMESTAMPS_H
 
 #include <vector>
 #include <unordered_map>
 #include <chrono>
 
-// can use const pair with clang++ but not g++
-typedef std::vector<std::pair<const std::string, const std::chrono::steady_clock::time_point> > ts_vector;
+// mark_time using nanotimer
+void mark_time(std::vector<std::tuple<std::string, double>> & timestamps, double & timestamp, std::string_view label);
 
-void mark_time(ts_vector& timestamps, std::string_view label);
-void print_timestamps(const ts_vector & timestamps, std::string_view filename, std::string_view device_name);
+// print_timestamps using nanotimer
+void print_timestamps(std::vector<std::tuple<std::string, double>> & timestamps);
 
-#endif // INTEGRATION_TIMESTAMPS_H
+#endif
